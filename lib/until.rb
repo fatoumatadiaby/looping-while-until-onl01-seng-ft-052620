8359e6a8d19d768e0ce6277d27827e1a82043e98
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-05-31 13:24:59 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,6 @@
 def using_until
   levitation_force = 6
-  while levitation_force < 10
+  until levitation_force == 10
 	puts "Wingardium Leviosa"
 	levitation_force += 1 
  end
@@ -8,3 +8,9 @@ end
 
 # Let's get the second test passing by coding our solution in `until.rb`:
 # Fill out the content of the `using_until` method to `puts` the desired phrase, "Wingardium Leviosa", until our levitation force equals `10`. Remember, every time you `puts` the phrase, you should increment your levitation force by `1`.
+counter = 0
+
+# until counter == 20
+#   puts "The current number is less than 20."
+#   counter += 1
+# end
